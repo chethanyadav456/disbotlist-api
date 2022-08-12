5907b71218d2d56392fc01e883e60118417a9450
Chethan Yadav <rockchethan1111@gmail.com>
2022-08-12 06:11:25 +0530

    chnaged the website url.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = class VOID {
   }
 
   serverCount(message) {
-  fetch(`https://disbotlist.xyz/api/bots/stats`, {
+  fetch(`https://disbotlist.top/api/bots/stats`, {
         method: 'POST',
         headers: { 
           'serverCount': this.client.guilds.cache.size,
@@ -19,14 +19,14 @@ module.exports = class VOID {
   }
   
   async search(id) {
-  return await fetch(`https://disbotlist.xyz/api/bots/${id}`, {
+  return await fetch(`https://disbotlist.top/api/bots/${id}`, {
         method: 'GET'
     })
     .then(res => res.json()).then(json => json);
   }
   
   async hasVoted(id) {
-  return await fetch(`https://disbotlist.xyz/api/bots/check/${id}`, {method: 'GET',headers: { 
+  return await fetch(`https://disbotlist.top/api/bots/check/${id}`, {method: 'GET',headers: { 
     'Content-Type': 'application/json', 'Authorization': this.token
   }
   }).then(res => res.json()).then(async json => json.voted);
